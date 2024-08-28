8d59b1b2fbe1e7521661c9b24ac679a749970195
Steven Holdsworth <sholdsworth@gmail.com>
2024-08-28 20:03:14 +0100

    done done

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -1,57 +1,38 @@
 defmodule GildedRose do
-  def update_quality(items) do
-    Enum.map(items, &update_item/1)
-  end
-
-  def update_item(item) do
-    item
-    |> update_item_quality()
-    |> update_item_sell_in()
-  end
-
+ 
   @backstage_passes ["Backstage passes to a TAFKAL80ETC concert"]
   @apreciators @backstage_passes ++ ["Aged Brie"]
   @conjured_items ["Spellbook"]
 
-  defp update_item_quality(item = %Item{name: "Sulfuras, Hand of Ragnaros"}), do: item
-
-  defp update_item_quality(item = %Item{name: name, quality: quality}) when name in @conjured_items do
-    %Item{item | quality: quality - 2}    
-  end 
-
-  defp update_item_quality(item = %Item{name: name, sell_in: sell_in}) 
-    when name in @backstage_passes and sell_in <= 0 do
-    %Item{item | quality: 0}
+  def update_quality(items) do
+    Enum.map(items, &update_item/1)
   end
 
-  defp update_item_quality(item = %Item{name: name, sell_in: sell_in, quality: quality}) 
-    when name in @backstage_passes and sell_in <= 5 do
-    %Item{item | quality: min(quality + 3, 50)}
+  defp update_item(item = %Item{sell_in: sell_in, quality: quality}) do
+    %Item{
+      item |
+        quality: update(:quality, quality, quality(item)),
+        sell_in: update(:sell_in, sell_in, sell_in(item))
+    }
   end
 
-  defp update_item_quality(item = %Item{name: name, sell_in: sell_in, quality: quality}) 
-    when name in @backstage_passes and sell_in <= 10 do
-    %Item{item | quality: min(quality + 2, 50)}
-  end
+  defp update(:quality, value, :noop), do: value
+  defp update(:quality, _value, :zero), do: 0
+  defp update(:quality, value, op), do: (value + op) |> min(50) |> max(0)
 
-  defp update_item_quality(item = %Item{name: name, quality: quality}) when name in @apreciators do
-    %Item{item | quality: min(quality + 1, 50)}
-  end
+  defp update(:sell_in, value, :noop), do: value
+  defp update(:sell_in, value, op), do: (value + op) 
 
-  defp update_item_quality(item = %Item{sell_in: sell_in, quality: quality})
-       when sell_in <= 0 and quality > 0 do
-    %Item{item | quality: max(quality - 2, 0)}
-  end
+  defp quality(%Item{name: "Sulfuras, Hand of Ragnaros"}), do: :noop 
+  defp quality(%Item{name: name}) when name in @conjured_items, do: -2
+  defp quality(%Item{name: name, sell_in: sell_in}) when name in @backstage_passes and sell_in <= 0, do: :zero
+  defp quality(%Item{name: name, sell_in: sell_in}) when name in @backstage_passes and sell_in <= 5, do: 3
+  defp quality(%Item{name: name, sell_in: sell_in}) when name in @backstage_passes and sell_in <= 10, do: 2
+  defp quality(%Item{name: name}) when name in @apreciators, do: 1 
+  defp quality(%Item{sell_in: sell_in, quality: quality}) when sell_in <= 0 and quality > 0, do: -2
+  defp quality(%Item{quality: quality}) when quality > 0, do: -1
+  defp quality(%Item{}), do: :noop
 
-  defp update_item_quality(item = %Item{quality: quality}) when quality > 0 do
-    %Item{item | quality: quality - 1}
-  end
-
-  defp update_item_quality(item), do: item
-
-  defp update_item_sell_in(item = %Item{name: "Sulfuras, Hand of Ragnaros"}), do: item
-
-  defp update_item_sell_in(item = %Item{sell_in: sell_in}) do
-    %Item{item | sell_in: sell_in - 1}
-  end
+  defp sell_in(%Item{name: "Sulfuras, Hand of Ragnaros"}), do: :noop
+  defp sell_in(%Item{}), do: -1
 end
